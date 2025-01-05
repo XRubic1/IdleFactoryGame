f62056ba2c2830b7c2d66f12e875fcf9a7e149d3
XRubic1 <petar.kostic388@gmail.com>
2025-01-05 05:43:35 +0100

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  basePath: '/IdleFactoryGame',
-  assetPrefix: '/IdleFactoryGame/',
-}
-
-module.exports = nextConfig 
