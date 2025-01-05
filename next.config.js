396dd90358e879bf715d26e6f956edc78a854bde
XRubic1 <petar.kostic388@gmail.com>
2025-01-05 05:49:31 +0100

    Correct page added

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/IdleFactoryGame',
+  assetPrefix: '/IdleFactoryGame/',
+  trailingSlash: true
+}
+
+module.exports = nextConfig
